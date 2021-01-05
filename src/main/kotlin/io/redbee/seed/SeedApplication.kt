da50b6dd1c37dd643c81c874d7c0065b35124819
guillermo grillo <guillermo.grillo88@gmail.com>
2021-01-05 12:56:04 -0300

    initial commit

---
+++ src/main/kotlin/io/redbee/seed/SeedApplication.kt
@@ -0,0 +1,11 @@
+package io.redbee.seed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeedApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeedApplication>(*args)
+}
