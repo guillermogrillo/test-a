2b10f54106629f82fa875ecb9320e0b77dfc0af5
guillermo grillo <guillermo.grillo88@gmail.com>
2021-01-06 10:39:14 -0300

    build gradle dependencies order

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,18 +16,21 @@ repositories {
 }
 
 dependencies {
-	//Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	//Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	//Springboot Test
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 
-	//mongodb
+
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+
+
+
+
+
 }
 
 tasks.withType<KotlinCompile> {
